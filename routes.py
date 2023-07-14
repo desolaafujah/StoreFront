7d646c26f957f4dd0e9828d99ca204f0d0cfeab4
Meredith Estrella Hernandez <mestrella@dhcp-194-114.student-wlan.bowdoin.edu>
2023-07-14 03:17:45 -0400

    table of products picked displayed on email

--- routes.py
+++ routes.py
@@ -1,29 +1,24 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
 from forms import RegistrationForm
 import smtplib
 from email.message import EmailMessage
 import ssl
+from bs4 import BeautifulSoup
+import requests
+import certifi
 
 app = Flask(__name__)
-click_count = {f'item{i+1}': 0 for i in range(8)}
-
+list_cards_string = '' # Define list_cards as a global variable
+updated_list_cards = []
 app.config['SECRET_KEY'] = '1324f97949e0f2c0cb404cbbfe9b9c9d'
 
 @app.route("/")
-@app.route("/home")
 def home():
+    return render_template('main.html')
+
+@app.route("/shop")
+def shop():
     return render_template('shopping.html')
-
-@app.route("/increment", methods=['POST'])
-def increment():
-    button_id = request.get_json().get('button_id')
-    if button_id in click_count:
-        click_count[button_id] += 1
-    return str(click_count[button_id])
-
-@app.route("/cart")
-def cart():
-    return render_template('result.html', numbers=click_count)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -35,27 +30,93 @@ def register():
         email_password = 'lenbbbooyivmubzq'
         email_receiver = form.email.data
         subject = 'Receipt for StoreFront'
+        
         body = f"""
             Thank you for Shopping with us!!
             This is your receipt.
         """
+
+        # Create the HTML table content
+
+        table_html = """
+        <table style="border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+        """
+
+        total_price = 0
+
+        for item in updated_list_cards:
+            name = item['name']
+            price = item['price']
+            quantity = item['quantity']
+
+            total_price += price * quantity
+
+            row_html = f"""
+            <tr>
+                <td>{name}</td>
+                <td>{price}</td>
+                <td>{quantity}</td>
+            </tr>
+            """
+
+            table_html += row_html
+
+        total_row_html = f"""
+        <tr>
+            <td>Total Price</td>
+            <td>{total_price}</td>
+            <td></td>
+        </tr>
+        """
+
+        table_html += total_row_html
+
+        table_html += """
+            </tbody>
+        </table>
+        """
+
+        body += table_html
+
         em = EmailMessage()
         em['From'] = email_sender
         em['To'] = email_receiver
         em['Subject'] = subject
-        em.set_content(body)
+        em.set_content(body, subtype='html')  # Set email content as HTML
 
-        context = ssl.create_default_context()
+        context = ssl.create_default_context(cafile=certifi.where())
+
         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
             smtp.login(email_sender, email_password)
             smtp.send_message(em)
-
-        click_count = {f'item{i+1}': 0 for i in range(8)}  
 
         return redirect(url_for('home'))  # if valid - send to home page
 
     return render_template('payment.html', title='Register', form=form)
 
+@app.route("/update_cart", methods=['POST'])
+def update_cart():
+    global list_cards_string
+    global updated_list_cards
+    data = request.json  # Retrieve the JSON data sent from the frontend
+    updated_list_cards = data  # Assign the updated data to a new list variable
+    list_cards_string = str(updated_list_cards)  # Convert the list to a string representation
+    return list_cards_string  # Return the updated list_cards data as a string
+
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8000)
-    
+
+
+
+
+
+
