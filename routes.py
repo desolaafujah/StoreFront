a2ddb0cf1199d32fe195b03b259173d37a10cd4c
Meredith Estrella Hernandez <mestrella@dhcp-194-114.student-wlan.bowdoin.edu>
2023-07-25 14:26:54 -0400

    Fixing errors

--- routes.py
+++ routes.py
@@ -56,57 +56,12 @@ def register():
         if existing_user:
             flash('Username is already taken. Please choose a different username.', 'danger')
         else:
-             email_setup(form)
-             return redirect(url_for('home'))  # if valid - send to home page 
-    return render_template('payment.html', title='Register', form=form)
-
-# Route to update the cart data
-@app.route("/update_cart", methods=['POST'])
-def update_cart():
-    #global list_cards_string
-    #global updated_list_cards
-    data = request.json  # Retrieve the JSON data sent from the frontend
-    session['cart'] = data  # Store the cart data in the session
-    return "Cart updated successfully", 200
-    #updated_list_cards = data  # Assign the updated data to a new list variable
-    #list_cards_string = str(updated_list_cards)  # Convert the list to a string representation
-    #return list_cards_string  # Return the updated list_cards data as a string
-
-
-@app.route("/log_in", methods=['GET', 'POST'])
-def log_in():
-    #create an instance of the log class
-    form = LogForm()
-    if form.validate_on_submit():
-        # This queries in the database to find the username and password with the provided username and password
-        user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
-        # if the username is found stores the user ID in the session and will be redirected to the main page  
-        if user:
-            email_setup(form)
-
-            flash('Email Sent! Check your inbox hottie :)')
-
-            session.pop('cart', None) 
-
-            return redirect(url_for('home'))  # if valid - send to home page
-        # if the username is not found then flash the message below
-        else:
-            flash('Invalid username or password. Please try again.', 'danger')
-    return render_template('log_in.html', subtitle='Log In', form=form)
-    
-# Auto-deployment route for PythonAnywhere
-@app.route("/update_server", methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/bodyfitapp/Project-2')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-
-def email_setup(form):
-    # Create a new user object and add it to the database
+            # Check if the email already exists in the database
+            existing_email = User.query.filter_by(email=form.email.data).first()
+            if existing_email:
+                flash('Email is already taken. Please choose a different email.', 'danger')
+            else:
+                # Create a new user object and add it to the database
                 new_user = User(
                     username=form.username.data,
                     email=form.email.data,
@@ -192,6 +147,130 @@ def email_setup(form):
                 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
                     smtp.login(email_sender, email_password)
                     smtp.send_message(em)
+
+                return redirect(url_for('home'))  # if valid - send to home page
+
+    return render_template('payment.html', title='Register', form=form)
+
+# Route to update the cart data
+@app.route("/update_cart", methods=['POST'])
+def update_cart():
+    #global list_cards_string
+    #global updated_list_cards
+    data = request.json  # Retrieve the JSON data sent from the frontend
+    session['cart'] = data  # Store the cart data in the session
+    return "Cart updated successfully", 200
+    #updated_list_cards = data  # Assign the updated data to a new list variable
+    #list_cards_string = str(updated_list_cards)  # Convert the list to a string representation
+    #return list_cards_string  # Return the updated list_cards data as a string
+
+
+@app.route("/log_in", methods=['GET', 'POST'])
+def log_in():
+    #create an instance of the log class
+    form = LogForm()
+    if form.validate_on_submit():
+        # This queries in the database to find the username and password with the provided username and password
+        user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+        # if the username is found stores the user ID in the session and will be redirected to the main page  
+        if user:
+            session['user_id'] = user.id
+            email_sender = 'meh.fruits@gmail.com'
+            email_password = 'lenbbbooyivmubzq'
+            email_receiver = user.email
+            subject = 'Receipt for StoreFront'
+            
+            body = f"""
+                Thank you for Shopping with us!!
+                This is your receipt.
+            """
+
+            # Create the HTML table content
+
+            table_html = """
+            <table style="border-collapse: collapse;">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+            """
+
+            total_price = 0
+
+            updated_list_cards = session.get('cart', []) 
+
+            for item in updated_list_cards:
+                if item != None: 
+                    name = item['name']
+                    price = item['price']
+                    quantity = item['quantity']
+
+                    total_price += price * quantity
+
+                    row_html = f"""
+                    <tr>
+                        <td>{name}</td>
+                        <td>{price}</td>
+                        <td>{quantity}</td>
+                    </tr>
+                    """
+
+                    table_html += row_html 
+
+            total_row_html = f"""
+            <tr>
+                <td>Total Price</td>
+                <td>{total_price}</td>
+                <td></td>
+            </tr>
+            """
+
+            table_html += total_row_html
+
+            table_html += """
+                </tbody>
+            </table>
+            """
+
+            body += table_html
+
+            em = EmailMessage()
+            em['From'] = email_sender
+            em['To'] = email_receiver
+            em['Subject'] = subject
+            em.set_content(body, subtype='html')  # Set email content as HTML
+
+            context = ssl.create_default_context(cafile=certifi.where())
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+                smtp.login(email_sender, email_password)
+                smtp.send_message(em)
+
+            flash('Email Sent! Check your inbox hottie :)')
+
+            session.pop('cart', None) 
+
+            return redirect(url_for('home'))  # if valid - send to home page
+        # if the username is not found then flash the message below
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
+    return render_template('log_in.html', subtitle='Log In', form=form)
+    
+# Auto-deployment route for PythonAnywhere
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/bodyfitapp/Project-2')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 # Define the User database model
 class User(db.Model):
